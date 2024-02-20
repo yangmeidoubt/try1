fd4f9b6a0e3c621bc72fd03519c207226c4fc2ab
yangmeidoubt <157636690+yangmeidoubt@users.noreply.github.com>
2024-02-20 18:39:45 +0800

    Add files via upload

---
+++ requests第一血.py
@@ -0,0 +1,10 @@
+import requests
+
+if __name__ == '__main__':
+    url = 'https://sogou.com/'
+    response = requests.get(url=url)
+    page_text = response.text
+    print(page_text)
+    with open('./sogou.html', 'w', encoding='utf-8') as fp:
+        fp.write(page_text)
+    print('爬取数据结束!!!')
